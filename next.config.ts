bf1f3bd94afa9d5f5703810797b2dfb28f6b0500
michellenieto5 <michellenieto5@hotmail.com>
2025-11-11 23:47:21 -0800

    Let's do this in Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,9 @@
 import type { NextConfig } from "next";
-const isPro = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
-  basePath: isPro ? '/minnies.work' : '',
-  assetPrefix: isPro ? '/minnies.work' : '',
-  output: 'export',
-  trailingSlash: true
+
 };
 
 export default nextConfig;
