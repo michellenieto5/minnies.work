840ff193f8cbcb305aec1d412f4192f22c9b1240
michellenieto5 <michellenieto5@hotmail.com>
2025-11-15 22:39:42 -0800

    Deleted InterestSection from page I BET THIS WAS THE BUG

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 import HeroSection from "@/components/HeroSection";
 import AboutSection from "@/components/AboutSection";
 import Projects from "@/components/Projects";
-import InterestsSection from "@/components/InterestsSection";
 
 export default function Home() {
   return (
